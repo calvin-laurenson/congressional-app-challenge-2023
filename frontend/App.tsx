0cb53c725b2c04ce75ef0b6fcf2fb94e43dc06d7
Calvin Laurenson <89622328+calvin-laurenson@users.noreply.github.com>
2023-09-24 11:38:21 -0700

    add navigation skeleton and web support

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,16 +1,26 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './pages/Home';
+
+const RootStack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Root />
     </NavigationContainer>
   );
+}
+
+function Root() {
+  const navigation = useNavigation();
+  return (
+    <RootStack.Navigator initialRouteName="Home">
+      <RootStack.Screen name="Home" component={Home} />
+    </RootStack.Navigator>
+  )
 }
 
 const styles = StyleSheet.create({
---
+++ frontend/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function Sidebar() {
+    return (
+        <View>
+          <Text>Sidebar</Text>
+        </View>
+    );
+  }
+  
