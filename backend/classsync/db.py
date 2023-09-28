e7d3142368d3479eabd2c68b07da65bf36aba6f0
James Escobedo <escobedo.james01@gmail.com>
2023-09-27 17:16:08 -0700

    Fix extraneous database features

--- backend/classsync/db.py
+++ backend/classsync/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Date, Integer, String, Float, Boolean
-from sqlalchemy.orm import declarative_base, sessionmaker
-
+from sqlalchemy.orm import declarative_base, mapped_column
+from pgvector.sqlalchemy import Vector
 engine = create_engine('sqlite:///app_data.db', echo=True)
 Base = declarative_base()
 
@@ -15,81 +15,20 @@ class Student(Base):
     id = Column(Integer, primary_key=True)
 
     name = Column(String)
-    face_id = Column(Integer)
-    class_id = Column(Float)
-    attendance = Column(Boolean)
-    date = Column(String)
+    face_embedding = Column(Vector(384))
 
-    def __init__(self, name, attendance, face_id, class_id, date):
+    def __init__(self, name, face_id):
         self.name = name
         self.face_id = face_id
-        self.attendance = attendance
-        self.class_id = class_id
-        self.date = date
-
-# Teacher Class takes name & class_id parameters
-
-class Teacher(Base):
-    __tablename__ = "teachers"
-
-    id = Column(Integer, primary_key=True)
-
-    name = Column(String)
-    class_id = Column(String)
-    username = Column(String)
-    password = Column(String)
-
-    def __init__(self, name, class_id, username, password):
-        self.name = name
-        self.class_id = class_id
-        self.username = username
-        self.password = password
-
-class Schedule(Base):
-    __tablename__ = 'schedules'
-
-    id = Column(Integer, primary_key=True)
-
-    school_name = Column(String)
-    schedule_id = Column(Integer)
-    periods = Column(Integer)
-
-    def __init__(self, school_name, schedule_id, periods):
-        self.school_name = school_name
-        self.schedule_id = schedule_id
-        self.periods = periods
-
-class Class(Base):
-    __tablename__ = 'classes'
-
-    id = Column(Integer, primary_key=True)
-
-    class_id = Column(Integer)
-    schedule_id = Column(Integer)
-    duration = Column(Integer)
-    periods = Column(String)
-
-    def __init__(self, class_id, schedule_id, duration, periods):
-        self.class_id = class_id
-        self.schedule_id = schedule_id
-        self.duration = duration
-        self.periods = periods
-
-### Next classes are user-configured timers
-
-# Timer class takes name, timing, and alarm sound id
 
 class Timer(Base):
     __tablename__ = "timers"
 
     id = Column(Integer, primary_key=True)
-
-    name = Column(String)
     timing = Column(Integer)
     alarm_sound_id = Column(String)
 
-    def __init__(self, name, timing, alarm_sound_id):
-        self.name = name
+    def __init__(self, timing, alarm_sound_id):
         self.timing = timing
         self.alarm_sound_id = alarm_sound_id
 
--- backend/classsync/main.py
+++ backend/classsync/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from classsync.db import Student, Teacher, Timer, Schedule, Class, engine
+from classsync.db import Student, Timer, engine
 from sqlalchemy.orm import Session
 import uvicorn
 
@@ -9,41 +9,17 @@ app = FastAPI()
 # Post section for students, teachers, timers
 
 @app.post("/add_student")
-async def add_student(name: str, face_id: int, class_id: float, date:str, attendance: bool = False):
+async def add_student(name, face_embedding):
     with Session(engine) as session:
-        student = Student(name, attendance, face_id, class_id, date)
+        student = Student(name, face_embedding)
         session.add(student)
         session.commit()
         return {"Student succesfully added" : student.name}
 
-@app.post("/add_teacher")
-async def add_teacher(name: str, class_id: int, username: str, password: str):
-    with Session(engine) as session:
-        teacher = Teacher(name, class_id, username, password)
-        session.add(teacher)
-        session.commit()
-        return {"Teacher succesfully added" : teacher.name}
-
-@app.post("/add_schedule")
-async def add_schedule(school_name: str, schedule_id: int, periods: int):
-    with Session(engine) as session:
-        schedule = Schedule(school_name, schedule_id, periods)
-        session.add(schedule)
-        session.commit()
-        return {"Schedule succesfully added" : schedule.schedule_id}
-    
-@app.post("/add_class")
-async def add_class(class_id: int, schedule_id: int, duration: int, periods: str):
-    with Session(engine) as session:
-        class_ = Class(class_id, schedule_id, duration, periods)
-        session.add(class_)
-        session.commit()
-        return {"Class succesfullly added" : class_.class_id}
-    
 @app.post("/add_timer")
-async def add_timer(name: str, timing: int, alarm_sound_id: str):
+async def add_timer(timing, alarm_sound_id):
     with Session(engine) as session:
-        timer = Timer(name, timing, alarm_sound_id)
+        timer = Timer(timing, alarm_sound_id)
         session.add(timer)
         session.commit()
         return {"Timer succesfully added" : timer.name}
@@ -55,24 +31,6 @@ async def get_student():
     with Session(engine) as session:
         student_query = session.query(Student)
         return student_query.all()
-
-@app.get("/get_teacher")
-async def get_teacher():
-    with Session(engine) as session:
-        teacher_query = session.query(Teacher)
-        return teacher_query.all()
-    
-@app.get("/get_schedule")
-async def get_schedule():
-    with Session(engine) as session:
-        schedule_query = session.query(Schedule)
-        return schedule_query.all()
-
-@app.get("/get_class")
-async def get_class():
-    with Session(engine) as session:
-        class_query = session.query(Class)
-        return class_query.all()
     
 @app.get("/get_timer")
 async def get_timer():
